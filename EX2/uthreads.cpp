9b6d8b83d90d9f51b4912cfe937607d62fa91226
galhar <gal.harari11@gmail.com>
2020-04-26 11:25:28 +0300

    merged wrapper into the uthreads.cpp

--- EX2/uthreads.cpp
+++ EX2/uthreads.cpp
@@ -1,11 +1,59 @@
 #include "uthreads.h"
 #include "wrapper.h"
 #include <sys/time.h>
+#include <stdio.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#inculde "uthreads.h"
+#include <queue>
+
 
 #define ARBITRARY_SIG 0
+#define BLOCKED 0
+#define RUNNING 1
+#define READY 2
+
+using namespace std;
 
 
-Wrapper:Wrapper wrapper();
+typedef struct Thread{
+    sigjmp_buf env;
+    char stack[STACK_SIZE];
+    int mode;
+    int id;
+    int quantsRanUntilNow;
+    int priority;
+} thread;
+
+
+// wrapper part:
+
+//TODO: initialize correctly
+thread threadArray[MAX_THREAD_NUM];
+int* quantumArray;
+int quantumArraySize;
+thread* runningThread;
+queue<thread> readyThreadsQueue;
+struct itimerval timer;
+
+
+void schedule(int sig){
+
+    // TODO: update the queue, and accrodingly the runnig
+
+    timer.it_value.tv_usec = quantumArray[runningThread.id];// first time interval, microseconds part
+    // Start a virtual timer. It counts down whenever this process is executing.
+    if (setitimer (ITIMER_VIRTUAL, &timer, NULL)) {
+        printf("setitimer error.");
+    }
+
+    printf("Scheduled\n");
+}
+
+// end of wrapper part
+
 
 /*
  * Description: This function initializes the thread library.
