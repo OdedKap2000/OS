8dc7131ff7bed507d5b975b022c84d97d4e045b4
odedkaplan <odedkaplan40@gmail.com>
2020-04-26 11:01:17 +0300

    Added EX2 base files

---
+++ EX2/wrapper.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#inculde "uthreads.h"
+#include <queue> 
+
+
+#define BLOCKED 0
+#define RUNNING 1
+#define READY 2
+
+using namespace std; 
+
+
+typedef struct Thread{
+    sigjmp_buf env;
+	char stack[STACK_SIZE];
+    int mode;
+	int id;
+	int quantsRanUntilNow;
+} thread;
+
+
+class Wrapper { 
+private: 
+    thread threadArray[MAX_THREAD_NUM]; 
+    int* quantumArray;
+	int quantumArraySize;
+	thread* runningThread;
+	queue<thread> readyThreadsQueue;
+	struct itimerval timer;
+	
+public: 
+	Wrapper() 
+    { 
+		//TODO: fill
+    }
+	
+	//TODO: create setters
+
+	void schedule(int sig){
+		
+		// TODO: update the queue, and accrodingly the runnig
+		
+		timer.it_value.tv_usec = quantumArray[runningThread.id];// first time interval, microseconds part
+		// Start a virtual timer. It counts down whenever this process is executing.
+		if (setitimer (ITIMER_VIRTUAL, &timer, NULL)) {
+			printf("setitimer error.");
+		}
+		
+		printf("Scheduled\n");
+	}
+}; 
+  
