92009f58c907c72822159a864e03052f5f4005af
odedkaplan <odedkaplan40@gmail.com>
2020-04-27 19:58:25 +0300

    many fixes and error throws

--- EX2/uthreads.cpp
+++ EX2/uthreads.cpp
@@ -1,13 +1,11 @@
 #include "uthreads.h"
-#include "wrapper.h"
 #include <sys/time.h>
-#include <stdio.h>
 #include <setjmp.h>
 #include <signal.h>
-#include <unistd.h>
-#include <sys/time.h>
-#inculde "uthreads.h"
+#include <iostream>
+//#include <unistd.h>
 #include <list>
+#include <new>
 
 #define MAIN_THREAD 0
 #define ARBITRARY_SIG 0
@@ -22,14 +20,30 @@
 #define MAIN_THREAD_PRIORITY 0
 #define DEFAULT_QUANTS_RAN 0
 #define nullptr ((void*)0)
+#define SYS_ERROR "system error: "
+#define LIB_ERROR "thread library error: "
+#define QUANTUM_POSITIVE "quantum values must be positive\n"
+#define THREAD_NUMBER_EXCEEDED "max thread number exceeded\n"
+#define INVALID_THREAD_ID "invalid thread id\n"
+#define BLOCKED_MAIN_THREAD "cannot block main thread\n"
+#define SIGNAL_MASKING_ERROR "signal masking error\n"
+#define SIGSETJMP_ERROR "sigsetjmp error\n"
+#define SETITIMER_ERROR "setitimer error\n"
+#define SIGLONGJMP_ERROR "siglongjmp error\n"
+#define SIGACTION_ERROR "sigaction error\n"
+#define BAD_ALLOC_ERROR "bad alloc\n"
+#define SIGEMPTYSET_ERROR "sigemptyset error\n"
 
 //TODO: make sure all system calls are checked and exits with SYSTEM_CALL_FAILURE
-//TODO: go over the includes
+//TODO: handle over 1M usecs
+//TODO: check if priority is over quantumArraySize. In funcs spawn, change priority.
+//TODO: check if tid is over MAX_THREAD_NUM in funcs ___
 
 using namespace std;
 
 
-typedef struct Thread{
+typedef struct Thread
+{
     sigjmp_buf env;
     char stack[STACK_SIZE];
     int mode;
@@ -41,56 +55,104 @@ typedef struct Thread{
 
 // wrapper part:
 
-thread* threadArray[MAX_THREAD_NUM];
-int* quantumArray;
+thread *threadArray[MAX_THREAD_NUM];
+int *quantumArray;
 int quantumArraySize;
-thread* runningThread;
-list<thread*> readyThreadsQueue;
+int sumQuantumRan;
+thread *runningThread;
+list<thread *> readyThreadsQueue;
 struct itimerval timer;
 
+// TODO mask signal in safeNew
+thread *safeNew()
+{
+    try
+    {
+        thread *newThread = new Thread;
+        return newThread;
+    }
+    catch (bad_alloc)
+    {
+        cerr << SYS_ERROR << BAD_ALLOC_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
+}
 
-int schedule(int sig){
+bool invaildTid(int tid)
+{
+    if ((tid >= MAX_THREAD_NUM) || (threadArray[tid] == nullptr))
+    {
+        cerr << LIB_ERROR << INVALID_THREAD_ID;
+        return true;
+    }
+    return false;
+}
+
+sigset_t blockTimer()
+{
+    sigset_t set;
+    if ((sigemptyset(&set) == FAILURE) || (sigaddset(&set, SIGVTALRM) == FAILURE) ||
+        (sigprocmask(SIG_BLOCK, &set, NULL);== FAILURE))
+    {
+        cerr << SYS_ERROR << SIGNAL_MASKING_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
+    return set;
+}
+
+void unblockTimer(set)
+{
+    if (sigprocmask(SIG_UNBLOCK, &set, NULL) == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGNAL_MASKING_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
+
+}
+
+// TODO: check if terminate crashes
+int schedule(int sig)
+{
     // The situation to get here is that : the timer is called in the running of a certain thread, or the thread made
-    // himself blocked. It means we need to save the env of the running thread, change it's mode to ready and add it to
+    // himself blocked or terminated. It means we need to save the env of the running thread, change it's mode to ready and add it to
     // the ready queue. Then set the running thread as a new one from the queue, and jump to it, and start it's timer,
     // and update that it has 1 more running time quant.
 
     // mask the sigalarm. It is unmasked inside the if that happens after "longjmp"
-    sigset_t set;
-    sigemptyset(&set);
-    sigaddset(&set, SIGVTALRM);
-    sigprocmask(SIG_BLOCK, &set, NULL);
-
+    sigset_t set = blockTimer();
 
     // saves the current system environment into the envelope
-    int ret_val = sigsetjmp(runningThread->env,NON_ZERO);
-
+    int ret_val = sigsetjmp(runningThread->env, NON_ZERO);
+    if (ret_val == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGSETJMP_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
     // if ret_val isn't zeroit means it got here from "longjmp" and we want to set the timer and that's it.
     // the logic is that if we got here from "longjmp" we already arranged all the queue and modes of the threads, and
     // just need to start the timer.
 
-    if ( ret_val != ZERO ){
+    if (ret_val != ZERO)
+    {
         // we got here from "longjmp". so just set the timer
         timer.it_value.tv_usec = quantumArray[runningThread->priority];// first time interval, microseconds part
         // Start a virtual timer. It counts down whenever this process is executing.
-        if (setitimer (ITIMER_VIRTUAL, &timer, NULL)) {
-            //TODO: delete the print and fflush here
-            printf("setitimer error.");
-            fflush(stdout);
+        if (setitimer(ITIMER_VIRTUAL, &timer, NULL))
+        {
+            cerr << SYS_ERROR << SETITIMER_ERROR;
             exit(SYSTEM_CALL_FAILURE);
         }
 
         // unmask the sigalarm
-        sigprocmask(SIG_UNBLOCK, &set, NULL);
-
-        printf("Scheduled\n");
+        unblockTimer(set);
         return SUCCESS;
     }
 
     // if we're here we already saved the system environment into the envelope
 
     // If we got here bacause the running thread is blocked don't add it to the ready queue
-    if ( runningThread->mode != BLOCKED){
+    if (runningThread->mode != BLOCKED)
+    {
         runningThread->mode = READY;
         readyThreadsQueue.push_back(runningThread);
     }
@@ -100,13 +162,31 @@ int schedule(int sig){
     runningThread = readyThreadsQueue.front();
     readyThreadsQueue.pop_front();
 
-    runningThread->mode=READY;
+    runningThread->mode = RUNNING;
+    sumQuantumRan++;
     runningThread->quantsRanUntilNow++;
-    siglongjmp(runningThread->env,NON_ZERO);
+    if (siglongjmp(runningThread->env, NON_ZERO) == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGLONGJMP_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
 }
 
 // end of wrapper part
 
+
+bool checkArrayPositive(int *quantum_usecs, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (quantum_usecs[i] <= 0)
+        {
+            cerr << LIB_ERROR << QUANTUM_POSITIVE;
+            return false;
+        }
+    }
+    return true;
+}
 
 /*
  * Description: This function initializes the thread library.
@@ -117,14 +197,14 @@ int schedule(int sig){
  * size - is the size of the array.
  * Return value: On success, return 0. On failure, return -1.
 */
-int uthread_init(int *quantum_usecs, int size){
-	quantumArray = quantum_usecs;
-	quantumArraySize = size;
+int uthread_init(int *quantum_usecs, int size)
+{
+    if (!checkArrayPositive(quantum_usecs, size))
+        return FAILURE;
+    quantumArray = quantum_usecs;
+    quantumArraySize = size;
 
-    thread* newThread = new Thread;
-
-    // the NON_ZERO in the secong arg is to save the signal mask
-    sigsetjmp(newThread->env, NON_ZERO);
+    thread *newThread = safeNew();
 
     newThread->quantsRanUntilNow = DEFAULT_QUANTS_RAN;
     newThread->priority = MAIN_THREAD_PRIORITY;
@@ -132,24 +212,26 @@ int uthread_init(int *quantum_usecs, int size){
     int newID = get_new_id();
     newThread->id = newID;
     threadArray[newID] = newThread;
+    sumQuantumRan = DEFAULT_QUANTS_RAN;
 
-	struct sigaction sa = {0};
+    struct sigaction sa = {0};
 
-	// Install timer_handler as the signal handler for SIGVTALRM.
-	sa.sa_handler = &schedule;
-	if (sigaction(SIGVTALRM, &sa,NULL) < 0) {
-	    //TODO: delete the print and fflush here
-		printf("sigaction error.");
-        fflush(stdout);
+    // Install timer_handler as the signal handler for SIGVTALRM.
+    sa.sa_handler = &schedule;
+    if (sigaction(SIGVTALRM, &sa, NULL) == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGACTION_ERROR;
         exit(SYSTEM_CALL_FAILURE);
-	}
+    }
 
-	schedule(ARBITRARY_SIG);
+    schedule(ARBITRARY_SIG);
+    return SUCCESS;
 }
 
-int get_new_id(){
+int get_new_id()
+{
     int i = 0;
-    while (threadArray[i] != nullptr)
+    while ((i < MAX_THREAD_NUM) && (threadArray[i] != nullptr))
         i++;
     return i;
 }
@@ -165,19 +247,34 @@ int get_new_id(){
  * Return value: On success, return the ID of the created thread.
  * On failure, return -1.
 */
-int uthread_spawn(void (*f)(void), int priority){
-    thread* newThread = new Thread;
-    sp = (address_t)newThread->stack + STACK_SIZE - sizeof(address_t);
-    pc = (address_t)f;
-    sigsetjmp(newThread->env, 1);
+int uthread_spawn(void (*f)(void), int priority)
+{
+    int newID = getNewID();
+    if (newID == MAX_THREAD_NUM)
+    {
+        cerr << LIB_ERROR << THREAD_NUMBER_EXCEEDED;
+        return FAILURE;
+    }
+
+    thread *newThread = safeNew();
+    sp = (address_t) newThread->stack + STACK_SIZE - sizeof(address_t);
+    pc = (address_t) f;
+    if (sigsetjmp(newThread->env, 1) == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGSETJMP_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
     (newThread->env->__jmpbuf)[JB_SP] = translate_address(sp);
     (newThread->env->__jmpbuf)[JB_PC] = translate_address(pc);
-    sigemptyset(&newThread->env->__saved_mask);
+    if (sigemptyset(&newThread->env->__saved_mask) == FAILURE)
+    {
+        cerr << SYS_ERROR << SIGEMPTYSET_ERROR;
+        exit(SYSTEM_CALL_FAILURE);
+    }
 
     newThread->quantsRanUntilNow = DEFAULT_QUANTS_RAN;
     newThread->priority = priority;
     newThread->mode = READY;
-    int newID = getNewID();
     newThread->id = newID;
     threadArray[newID] = newThread;
 
@@ -193,13 +290,17 @@ int uthread_spawn(void (*f)(void), int priority){
  * next time the thread gets scheduled.
  * Return value: On success, return 0. On failure, return -1.
 */
-int uthread_change_priority(int tid, int priority){
+int uthread_change_priority(int tid, int priority)
+{
+    if (invaildTid(tid))
+        return FAILURE;
     threadArray[tid]->priority = priority;
     return SUCCESS;
 }
 
 
-int terminate_program(){
+int terminate_program()
+{
     for (int i = 0; i < MAX_THREAD_NUM; ++i)
     {
         if (threadArray[i] != nullptr)
@@ -221,15 +322,27 @@ int terminate_program(){
  * terminated and -1 otherwise. If a thread terminates itself or the main
  * thread is terminated, the function does not return.
 */
-int uthread_terminate(int tid){
-    thread* currThread = threadArray[tid];
+int uthread_terminate(int tid)
+{
+    if (invaildTid(tid))
+        return FAILURE;
+
+    if (tid == MAIN_THREAD)
+    {
+        sigset_t set = blockTimer();
+        terminate_program();
+    }
+
+    if (runningThread == currThread)
+    {
+        sigset_t set = blockTimer();
+    }
+    thread *currThread = threadArray[tid];
     readyThreadsQueue.remove(currThread);
     delete currThread;
     threadArray[tid] = nullptr;
-    if (tid == MAIN_THREAD){
-        terminate_program();
-    }
-    if (runningThread == currThread){
+    if (runningThread == currThread)
+    {
         schedule(ARBITRARY_SIG);
     }
 }
@@ -244,12 +357,27 @@ int uthread_terminate(int tid){
  * effect and is not considered an error.
  * Return value: On success, return 0. On failure, return -1.
 */
-int uthread_block(int tid){
-    if (tid == 0)
+int uthread_block(int tid)
+{
+    if (invaildTid(tid))
         return FAILURE;
-    thread* currThread = threadArray[tid];
+
+    if (tid == MAIN_THREAD)
+    {
+        cerr << LIB_ERROR << BLOCKED_MAIN_THREAD;
+        return FAILURE;
+    }
+
+    thread *currThread = threadArray[tid];
+
+    if (runningThread == currThread)
+    {
+        sigset_t set = blockTimer();
+    }
+
     currThread->mode = BLOCKED;
-    if (runningThread == currThread){
+    if (runningThread == currThread)
+    {
         schedule(ARBITRARY_SIG);
         return SUCCESS;
     }
@@ -265,8 +393,12 @@ int uthread_block(int tid){
  * ID tid exists it is considered an error.
  * Return value: On success, return 0. On failure, return -1.
 */
-int uthread_resume(int tid){
-    thread* currThread = threadArray[tid];
+int uthread_resume(int tid)
+{
+    if (invaildTid(tid))
+        return FAILURE;
+
+    thread *currThread = threadArray[tid];
     if (currThread->mode != BLOCKED)
         return SUCCESS;
     currThread->mode = READY;
@@ -279,7 +411,8 @@ int uthread_resume(int tid){
  * Description: This function returns the thread ID of the calling thread.
  * Return value: The ID of the calling thread.
 */
-int uthread_get_tid(){
+int uthread_get_tid()
+{
     return runningThread->id;
 }
 
@@ -292,13 +425,9 @@ int uthread_get_tid(){
  * should be increased by 1.
  * Return value: The total number of quantums.
 */
-int uthread_get_total_quantums(){
-    int sum = 0;
-    for (int i = 0; i < MAX_THREAD_NUM; ++i){
-        if (threadArray[i] != nullptr)
-            sum += threadArray[i]->quantsRanUntilNow;
-    }
-    return sum;
+int uthread_get_total_quantums()
+{
+    return sumQuantumRan;
 }
 
 
@@ -312,6 +441,9 @@ int uthread_get_total_quantums(){
  * Return value: On success, return the number of quantums of the thread with ID tid.
  * 			     On failure, return -1.
 */
-int uthread_get_quantums(int tid){
+int uthread_get_quantums(int tid)
+{
+    if (invaildTid(tid))
+        return FAILURE;
     return threadArray[tid]->quantsRanUntilNow;
 }
