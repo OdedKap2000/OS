9302386e293bad8741cf45f90465bd17e1597d2d
odedkaplan <odedkaplan40@gmail.com>
2020-06-15 00:12:13 +0300

    Start of EX4

---
+++ EX4/VirtualMemory.cpp
@@ -0,0 +1,77 @@
+#include "VirtualMemory.h"
+#include "PhysicalMemory.h"
+#include <cmath>
+
+
+void clearTable(uint64_t frameIndex)
+{
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i)
+    {
+        PMwrite(frameIndex * PAGE_SIZE + i, 0);
+    }
+}
+
+void VMinitialize()
+{
+    clearTable(0);
+}
+
+uint64_t findFreeFrame(uint64_t previousAddress)
+{
+    // Find an unused frame or evict a page from some frame.
+    // Make sure it's not the previous addresss
+
+}
+
+uint64_t getCurrentOffset(unsigned int currentLayer, uint64_t virtualAddress)
+{
+//    std::pow(2, OFFSET_WIDTH) - 1) is 1111..11 in length of OFFSET_WIDTH
+//    std::pow(2, (TABLES_DEPTH - currentLayer)) is the multiplier of the mask so it would match the current offset
+    uint64_t mask = ((1LL << OFFSET_WIDTH) - 1) << (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
+    uint64_t maskedOffset = virtualAddress & mask;
+    return maskedOffset >> (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
+
+}
+
+int VMread(uint64_t virtualAddress, word_t *value)
+{
+    uint64_t addr = 0;
+    word_t pmValue;
+    unsigned int currentLayer = 0;
+    word_t previousAddress = 0;
+    uint64_t freeFrame;
+    while (currentLayer < TABLES_DEPTH)
+    {
+        uint64_t currentOffset = getCurrentOffset(currentLayer, virtualAddress);
+        PMread(addr * PAGE_SIZE + currentOffset, &pmValue);
+        if (pmValue == 0)
+        {
+            freeFrame = findFreeFrame(previousAddress);
+
+            // Restore the page we are looking for to frame <free_frame> (only necessary in actual pages)
+            if (currentLayer < TABLES_DEPTH - 1)
+            {
+                for (int i = 0; i < ; ++i)
+                {
+
+                }
+            }
+
+            // Write 0 in all of its contents (only necessary in tables)
+
+            // PMwrite(addr * PAGE_SIZE + offset, <free_frame>)
+            //addr = <free_frame>
+
+        }
+        previousAddress = pmValue;
+        currentLayer++;
+    }
+    // If its a write command: PMwrite(addr * PAGE_SIZE + lastOffset, value)
+    return 1;
+}
+
+
+int VMwrite(uint64_t virtualAddress, word_t value)
+{
+    return 1;
+}
