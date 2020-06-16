5d6fa733552e27fa8af4fb1e58d66004b16ac744
oded kaplan <odedkaplan40@gmail.com>
2020-06-16 19:24:26 +0300

    created wrapper and started implementing DFS

--- EX4/VirtualMemory.cpp
+++ EX4/VirtualMemory.cpp
@@ -2,6 +2,9 @@
 #include "PhysicalMemory.h"
 #include <cmath>
 
+#define READ 1
+#define WRITE 2
+#define ROOT_OFFSET_WIDTH (VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH)
 
 void clearTable(uint64_t frameIndex)
 {
@@ -16,10 +19,26 @@ void VMinitialize()
     clearTable(0);
 }
 
+struct dfs_DATA{
+    uint64_t previousAddress;
+    int currentLayer;
+    int maxCyclicDistance;
+    uint64_t farthestAddressByCyclic;
+};
+
+uint64_t DFS(uint64_t previousAddress, int currentLayer, int maxFrameUsed, int maxCyclicDistance,
+             uint64_t farthestAddressByCyclic)
+{
+
+}
+
 uint64_t findFreeFrame(uint64_t previousAddress)
 {
     // Find an unused frame or evict a page from some frame.
     // Make sure it's not the previous addresss
+    dfs_DATA &dfs_data = dfs_DATA{.previousAddress=previousAddress, .currentLayer=0, .farthestAddressByCyclic = 0, .maxCyclicDistance 0,};
+    DFS()
+
 
 }
 
@@ -27,20 +46,28 @@ uint64_t getCurrentOffset(unsigned int currentLayer, uint64_t virtualAddress)
 {
 //    std::pow(2, OFFSET_WIDTH) - 1) is 1111..11 in length of OFFSET_WIDTH
 //    std::pow(2, (TABLES_DEPTH - currentLayer)) is the multiplier of the mask so it would match the current offset
+    if (currentLayer == 0)
+    {
+        uint64_t mask = ((1LL << ROOT_OFFSET_WIDTH) - 1) << (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
+        uint64_t maskedOffset = virtualAddress & mask;
+        return maskedOffset >> (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
+    }
     uint64_t mask = ((1LL << OFFSET_WIDTH) - 1) << (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
     uint64_t maskedOffset = virtualAddress & mask;
     return maskedOffset >> (OFFSET_WIDTH * (TABLES_DEPTH - currentLayer));
 
 }
 
-int VMread(uint64_t virtualAddress, word_t *value)
+int operationWrapper(uint64_t virtualAddress, word_t *value, int operation)
 {
+
     uint64_t pageID = virtualAddress >> OFFSET_WIDTH;
     uint64_t addr = 0;
     word_t pmValue;
     unsigned int currentLayer = 0;
-    word_t previousAddress = 0;
+    uint64_t previousAddress = 0;
     uint64_t freeFrame;
+    word_t maxFrameUsed = 0;
     while (currentLayer < TABLES_DEPTH)
     {
         uint64_t currentOffset = getCurrentOffset(currentLayer, virtualAddress);
@@ -48,7 +75,10 @@ int VMread(uint64_t virtualAddress, word_t *value)
         if (pmValue == 0)
         {
             freeFrame = findFreeFrame(previousAddress);
-
+            if (freeFrame > maxFrameUsed)
+            {
+                maxFrameUsed = freeFrame;
+            }
             if (currentLayer < TABLES_DEPTH - 1)
             {
                 clearTable(freeFrame);
@@ -57,16 +87,28 @@ int VMread(uint64_t virtualAddress, word_t *value)
                 PMrestore(freeFrame, pageID);
             }
         }
-        previousAddress = pmValue;
+        previousAddress = addr;
+        addr = pmValue;
         currentLayer++;
     }
-    PMread(addr*PAGE_SIZE +)
+    if (operation == READ)
+    {
+        PMread(addr * PAGE_SIZE + getCurrentOffset(TABLES_DEPTH, virtualAddress), value);
+    } else if (operation == WRITE)
+    {
+        PMwrite(addr * PAGE_SIZE + getCurrentOffset(TABLES_DEPTH, virtualAddress), *value);
+    }
     // If its a write command: PMwrite(addr * PAGE_SIZE + lastOffset, value)
     return 1;
 }
 
 
+int VMread(uint64_t virtualAddress, word_t *value)
+{
+    return operationWrapper(virtualAddress, value, READ);
+}
+
 int VMwrite(uint64_t virtualAddress, word_t value)
 {
-    return 1;
+    return operationWrapper(virtualAddress, &value, WRITE);
 }
